97b05972bc8e536b7d0b4efaabad21b3ad6e0a7f
leejuhyeok <tyrfingr12@naver.com>
2020-02-04 18:01:57 +0900

    content를 LongText 타입으로 변경

--- src/main/java/com/leejuhyeok/board/domain/Board.java
+++ src/main/java/com/leejuhyeok/board/domain/Board.java
@@ -7,7 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import lombok.Builder;
@@ -26,9 +28,10 @@ public class Board {
 	private Long idx;
 	
 	@Column
-	@NotNull
+	@NotBlank
 	private String title;
 	
+	@Lob
 	@Column
 	private String content; 
 	
