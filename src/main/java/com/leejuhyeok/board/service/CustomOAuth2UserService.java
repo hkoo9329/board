556bd89e04b04fc8d3cfafa8694f4006700cc130
leejuhyeok <tyrfingr12@naver.com>
2020-02-05 18:07:51 +0900

    소셜 로그인 기능 추가

---
+++ src/main/java/com/leejuhyeok/board/service/CustomOAuth2UserService.java
@@ -0,0 +1,61 @@
+package com.leejuhyeok.board.service;
+
+import java.util.Collections;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import com.leejuhyeok.board.config.OAuthAttributes;
+import com.leejuhyeok.board.domain.SessionUser;
+import com.leejuhyeok.board.domain.User;
+import com.leejuhyeok.board.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User>{
+	
+	private final UserRepository userRepository;
+	private final HttpSession httpSession;
+	
+	@Override
+	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+		// TODO Auto-generated method stub
+		
+		OAuth2UserService delegate = new DefaultOAuth2UserService();
+		OAuth2User oAuth2User = delegate.loadUser(userRequest);
+		
+		String registrationId = userRequest.getClientRegistration().getRegistrationId();
+		String userNameAttributeName = userRequest.getClientRegistration()
+				.getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+		
+		OAuthAttributes attributes = OAuthAttributes
+				.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+		User user = saveOrUpdate(attributes);
+		httpSession.setAttribute("user", new SessionUser(user));
+		
+		return new DefaultOAuth2User(
+				Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
+				attributes.getAttributes(),
+				attributes.getNameAttributeKey());
+				
+	}
+
+	private User saveOrUpdate(OAuthAttributes attributes) {
+		// TODO Auto-generated method stub
+		User user = userRepository.findByEmail(attributes.getEmail())
+				.map(entity -> entity.update(attributes.getNickName(), attributes.getPicture()))
+				.orElse(attributes.toEntity());
+		return userRepository.save(user);
+	}
+
+}
