20b0c66992cc11a3c85a8cf183252c161ed909e8
leejuhyeok <tyrfingr12@naver.com>
2020-02-05 18:15:41 +0900

    불필요 부분 수정

--- src/main/java/com/leejuhyeok/board/repository/CommentRepository.java
+++ src/main/java/com/leejuhyeok/board/repository/CommentRepository.java
@@ -1,6 +1,8 @@
 package com.leejuhyeok.board.repository;
 
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
@@ -12,6 +14,6 @@ import com.leejuhyeok.board.domain.Comment;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long>{
-	Optional<Comment> findByBoardIdx(Long boardIdx);
+	List<Comment> findByBoardIdx(Long boardIdx);
 	Optional<Comment> findByIdxAndBoardIdx(Long id, Long boardIdx);
 }
